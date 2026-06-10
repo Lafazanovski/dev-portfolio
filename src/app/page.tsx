37c9407e8b81d1aa7fd738883e432be9363ce775
Kristijan Lafazanovski <lafazanovski.kristijan1993@gmail.com>
2026-06-10 12:42:30 +0200

    update folder components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "./components/Hero/hero";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <main>
-     <Hero />
+      <Hero />
     </main>
   );
 }
